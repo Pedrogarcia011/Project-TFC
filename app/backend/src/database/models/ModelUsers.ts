7b6884fe02890c8c49fe958505a4af11cb971717
Pedrogarcia011 <pedrohenriquegracia011@gmail.com>
2023-11-08 11:09:36 -0300

    Feat: desenvolvendo migration e model de users, req06

---
+++ app/backend/src/database/models/ModelUsers.ts
@@ -0,0 +1,52 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  CreationOptional,
+  InferCreationAttributes,
+} from 'sequelize';
+import db from '.';
+
+class SequelizeUsers extends Model<InferAttributes<SequelizeUsers>,
+InferCreationAttributes<SequelizeUsers>> {
+  declare id: CreationOptional<number>;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+SequelizeUsers.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  timestamps: false,
+  sequelize: db,
+  modelName: 'users',
+});
+
+export default SequelizeUsers;
