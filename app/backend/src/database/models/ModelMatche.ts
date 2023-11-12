6610a01dbe13ed65f90efe689d097c498ac7a528
Pedrogarcia011 <pedrohenriquegracia011@gmail.com>
2023-11-12 11:01:05 -0300

    Feat: desenvolvendo migration e model para tabela departidas "matches", req13

---
+++ app/backend/src/database/models/ModelMatche.ts
@@ -0,0 +1,66 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+} from 'sequelize';
+import db from '.';
+
+class SequelizeMatchs extends Model<InferAttributes<SequelizeMatchs>,
+InferCreationAttributes<SequelizeMatchs>> {
+  declare id: number;
+  declare homeTeamGoals: number;
+  declare homeTeamId: number;
+  declare awayTeamGoals: number;
+  declare awayTeamId: number;
+  declare inProgress: boolean;
+}
+
+SequelizeMatchs.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+
+  homeTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'home_team_id',
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'home_team_goals',
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'away_team_goals',
+  },
+  awayTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'away_team_id',
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    field: 'in_progress',
+  },
+}, {
+  timestamps: false,
+  sequelize: db,
+  modelName: 'matches',
+});
+
+export default SequelizeMatchs;
